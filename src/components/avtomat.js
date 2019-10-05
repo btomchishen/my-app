0cfa72c02c1513f76aa46cef5d70582cf3931f71
btomchishen <btomchishen@icloud.com>
2019-10-05 21:42:27 +0300

    add club, vistavki, shop

--- src/components/avtomat.js
+++ src/components/avtomat.js
@@ -9,36 +9,35 @@ class Avtomatf extends React.Component {
                         АВТОМАТИ З СПЕЦІАЛЬНИМИ ПАКЕТАМИ
                 </div>
 
-                        <div className="info">
-                            &emsp;Прибери за своїм улюбленцем! До такого закликають власників чотирилапих друзів, які відвідують з ними парк Чекмана. Для нагадування необхідності прибрати за собакою встановили знак. Знизу поставили урну, в яку потрібно викидати фекалії тварин.  Також на стовпі прикріплений ящичок із пакетами, які можна використати для какашок.
+                    <div className="info">
+                        &emsp;Прибери за своїм улюбленцем! До такого закликають власників чотирилапих друзів, які відвідують з ними парк Чекмана. Для нагадування необхідності прибрати за собакою встановили знак. Знизу поставили урну, в яку потрібно викидати фекалії тварин.  Також на стовпі прикріплений ящичок із пакетами, які можна використати для какашок.
                 <p className="accept">Такі спеціально обладнанні смітники встановлені в наступних місцях:</p>
-                        </div>
-                        <div className="content">
-                            <div className="content-flex">
-                                <div className="content-photo-avtomat">
-                                    <img src={require("../img/avtomat/avtomat.jpeg")} />
-                                </div>
-                                <div className="content-text">
-                                    <div className="content-header">
-                                       Автомати з спеціальними пакетами
+                    </div>
+                    <div className="content">
+                        <div className="content-flex">
+                            <div className="content-photo-avtomat">
+                                <img src={require("../img/avtomat/avtomat.jpeg")} />
                             </div>
-                                    <div className="content-location">
-                                        Розташування: <strong>Парк ім. Михайла Чекмана</strong>
+                            <div className="content-text">
+                                <div className="content-header">
+                                    Автомати з спеціальними пакетами
                                     </div>
-
+                                <div className="content-location">
+                                    Розташування: <strong>Парк ім. Михайла Чекмана</strong>
                                 </div>
+                                <div className="content-photo-avtomat-2"><img src={require("../img/avtomat/checkman.jpeg")} /></div>
                             </div>
                         </div>
-
-
-
-
-                        <div className="home-footer">
-                            Copyright Bohdan Tomchishen 2019
                     </div>
+
+
+
+
+                    <div className="home-footer">
+                        Copyright Bohdan Tomchishen 2019
                     </div>
-                    <a href="https://vsim.ua/Podii/urnu-dlya-sobachih-fekaliy-vstanovili-v-parku-chekmana-10661645.html">Avtomat</a>
                 </div>
+            </div>
         )
     }
 }
--- src/components/shop.js
+++ src/components/shop.js
@@ -1,10 +1,74 @@
 import React from 'react'
 
-class Shopf extends React.Component{
-    render(){
-        return(
+class Shopf extends React.Component {
+    render() {
+        return (
             <div>
-                Shop
+                <div>
+                    <div className="header">
+                        АВТОМАТИ З СПЕЦІАЛЬНИМИ ПАКЕТАМИ
+                </div>
+
+                    <div className="info">
+                        &emsp;Спеціалізовані магазини займаються продажем кормів, іграшок, домівок, ошейників та поводків, засобів для догляду за вашими домашніми улюбленцями та багато іншого. Також іноді зоомагазини надають різноманітні послуги від підрізання кігтів до обробки від кліщів та паразитів.
+                <p className="accept">Про такі спеціалізовані зоомагазини ви можете прочитати нижче:</p>
+                    </div>
+
+                    <div className="content">
+                        <div className="content-flex">
+                            <div className="content-photo">
+                                <img src={require("../img/shop/friend.jpg")} />
+                            </div>
+                            <div className="content-text-friend">
+                                <div className="content-header">
+                                    ЗооМагазин "Вірні друзі"
+                                    </div>
+                                <div className="content-location-friend">
+                                    <ul>До ваших послуг у зоомагазині «Вірні друзі»:
+                                    <li>корм для тварин, птахів, рибок;</li>
+                                        <li>сухий корм на вагу;</li>
+                                        <li>іграшки та аксесуари для домашніх улюбленців;</li>
+                                        <li>спеціальні будиночки для тварин;</li>
+                                        <li>великий вибір повідців та ошийників;</li>
+                                        <li>засоби по догляду за тваринами;</li>
+                                        <li>різноманітні ласощі та «смаколики» для тварин.</li>
+                                    </ul>
+                                    Розташування: <strong>вул. Панаса Мирного 4/1</strong><br />
+                                    Номер телефону: +38-067-593-2220
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="content">
+                        <div className="content-flex">
+                            <div className="content-photo">
+                                <img src={require("../img/shop/kitpes.png")} />
+                            </div>
+                            <div className="content-text-friend">
+                                <div className="content-header">
+                                    ЗооМагазин "КІТ-ПЕС"
+                                    </div>
+                                <div className="content-location-friend">
+                                    <ul>До ваших послуг у зоомагазині «КІТ-ПЕС»:
+                                    <li>корми для різних видів тварин (для кішок, котів, папужок, рептилій, черепах та ін.);</li>
+                                        <li>вітаміни та ліки для тварин;</li>
+                                        <li>акваріуми, тераріуми, клітки, «будиночки» для псів чи котів тощо;</li>
+                                        <li>усілякі веселі іграшки та корисні аксесуари для тварин;</li>
+                                        <li>великий вибір повідців та ошийників.</li>
+                                    </ul>
+                                    Розташування: <strong>Старокостянтинівське шосе 3</strong><br />
+                                    Номер телефону: +38-067-900-0613 +38-097-119-7570
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="home-footer">
+                        Copyright Bohdan Tomchishen 2019
+                    </div>
+                </div>
             </div>
         )
     }
